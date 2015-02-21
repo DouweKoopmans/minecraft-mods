5497350fa46c66eeb362b924b2fea250975635c7
vrackfall <vrackfall@gmail.com>
2015-02-21 17:08:44 -0500

    Scuse guys, I've removed this : The author of the mod will post they soon

--- GT/1.7.10/src/main/java/com/domochevsky/tonic/ClientProxy.java
+++
@@ -1,7 +0,0 @@
-package com.domochevsky.tonic;
-
-public class ClientProxy
-  extends CommonProxy
-{
-  public void registerRenderers() {}
-}
--- GT/1.7.10/src/main/java/com/domochevsky/tonic/CommonProxy.java
+++
@@ -1,6 +0,0 @@
-package com.domochevsky.tonic;
-
-public class CommonProxy
-{
-  public void registerRenderers() {}
-}
